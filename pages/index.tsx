220c3fb18a8982dd8aaf1e5191e45928ff1d82d6
Chantharojwong Kasidis <me@ckasidis.com>
2022-06-10 16:50:30 +0800

    add HomePage content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import { NextPage } from 'next';
-import { Center } from '@chakra-ui/react';
+import { Button, Center, Heading, Stack } from '@chakra-ui/react';
 
 interface HomePageProps {}
 
 const HomePage: NextPage<HomePageProps> = ({}) => {
 	return (
-		<Center as="section" maxW="md" h="100vh" mx="auto">
-			Home Page
+		<Center as="section" maxW="lg" h="100vh" mx="auto">
+			<Stack spacing="4" w="75%">
+				<Heading as="h1" size="md" textAlign="center">
+					Truth or Dare
+				</Heading>
+				<Stack spacing="2">
+					<Button colorScheme="brand" variant="solid">
+						New Game
+					</Button>
+					<Button colorScheme="brand" variant="outline">
+						Resume
+					</Button>
+				</Stack>
+			</Stack>
 		</Center>
 	);
 };
